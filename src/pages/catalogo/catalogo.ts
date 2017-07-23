8f67bb00bea795c0e00cb36839f47b902c79e91c
Allison Verdam da Cunha <allisonverdam@gmail.com>
2017-07-23 00:15:24 -0300

    catalogo

--- src/pages/catalogo/catalogo.ts
+++ src/pages/catalogo/catalogo.ts
@@ -2,20 +2,36 @@ import { Component } from '@angular/core';
 import { NavController, NavParams, IonicPage, ModalController } from 'ionic-angular';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
-@IonicPage()
+@IonicPage({
+  name: 'CatalogoPage',
+  segment: 'catalogo/:conta'
+})
 @Component({
   selector: 'page-catalogo',
   templateUrl: 'catalogo.html'
 })
 
 export class CatalogoPage {
-  produtos: FirebaseListObservable<any[]>
-  showItens: boolean = false
   categorias: any[]
   order = []
+  showItens: boolean = false;
+  produtos: FirebaseListObservable<any[]>;
+  conta: FirebaseListObservable<any[]>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public modalCtrl: ModalController, private db: AngularFireDatabase) {
+    if (navParams.get('conta')) {
+      this.conta = this.db.list('contas/' + navParams.get('conta'), {
+        query: {
+          equalTo: navParams.get('conta')
+        }
+      });
+      this.conta.subscribe(conta => {
+        if (conta.length === 0) {
+          this.conta.$ref.ref.update({ situacao: "aberta", precoTotal: 10, produtos: [] })
+        }
+      })
+    }
 
   }
 
