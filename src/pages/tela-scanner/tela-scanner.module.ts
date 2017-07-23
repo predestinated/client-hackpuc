49ba16c315ebdc4101b35850f7f2132ca5272330
Allison Verdam da Cunha <allisonverdam@gmail.com>
2017-07-23 04:45:13 -0300

    adicionando plugin do scanner e iniciando a tela

---
+++ src/pages/tela-scanner/tela-scanner.module.ts
@@ -0,0 +1,9 @@
+import { TelaScannerPage } from './tela-scanner';
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+
+@NgModule({
+  declarations: [TelaScannerPage],
+  imports: [IonicPageModule.forChild(TelaScannerPage)],
+})
+export class TelaScannerPageModule { }
